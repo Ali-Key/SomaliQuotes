42fddcac844240dbac55c7af631000003a4b931e
Ali Omar Abdi <Mafiobio700@gmail.com>
2023-08-29 22:13:49 +0300

    useState react i maked somali quotes

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SomaliQuotes from './SomaliQuotes/SomaliQuotes'
+import Header from './Header/Header'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <SomaliQuotes />
+    </>
+  )
+}
+
+export default App
---
+++ my-project/src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className=" text-center  font-bold bg-blue-600 text-2xl text-white p-10 uppercase "
+     
+    >
+      Somali Quotes
+    </div>
+  );
+}
